08516055ae10edb3616e6a5b96b70f5dbc36ea2e
Rafael Biassio <rafaelobiassio@gmail.com>
2020-03-05 07:22:49 -0400

    Init project with Table and Button component

---
+++ register-people/src/components/Button.vue
@@ -0,0 +1,37 @@
+<template>
+  <md-button :class="getStyleClass" @click="onClick" :disabled="disabled">
+    <md-icon v-if="showIcon">add</md-icon>
+    {{ !showIcon && label }}
+  </md-button>
+</template>
+
+<script>
+export default {
+  props: {
+    label: String,
+    disabled: Boolean,
+    classType: String,
+    onClick: {
+      type: Function,
+      required: true
+    }
+  },
+  computed: {
+    getStyleClass() {
+      switch (this.$props.classType) {
+        case "primary":
+          return "md-dense md-raised md-primary";
+        case "accent":
+          return "md-dense md-raised md-accent";
+        case "fab":
+          return "md-fab md-primary";
+        default:
+          return "md-raised";
+      }
+    },
+    showIcon() {
+      return this.$props.classType === "fab";
+    }
+  }
+};
+</script>
---
+++ register-people/src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Table context="Pessoas" />  
+    <Button :onClick="handleClick" label="Click me" :disabled="block" classType="primary"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+import Table from "@/components/Table.vue";
+import Button from "@/components/Button.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld,
+    Table,
+    Button
+  },
+  methods: {
+    handleClick() {
+      alert("Works!");
+    }
+  },
+  data() {
+    return {
+      block: false
+    };
+  }
+};
+</script>
